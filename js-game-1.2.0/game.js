6c6d191c4786d2001afb286ba37d23d890331869
MG <mg84@bk.ru>
2017-09-20 23:28:05 +0300

    implemented class Vector

--- js-game-1.2.0/game.js
+++ js-game-1.2.0/game.js
@@ -4,3 +4,32 @@ loadLevels()
   .then(result => {
     return JSON.parse(result);
   });
+
+class Vector {
+  constructor(startX = 0, startY = 0) {
+    this.x = startX;
+    this.y = startY;
+  }
+  plus(vector) {
+    if(!(vector instanceof Vector)) {
+      throw `${vector} не является прототипом Vector`
+    }
+    let newVector = new Vector(this.x, this.y);
+    newVector.x += vector.x;
+    newVector.y += vector.y;
+    return newVector;
+  }
+  times(mult) {
+    let newVector = new Vector(this.x, this.y);
+    newVector.x *= mult;
+    newVector.y *= mult;
+    return newVector;
+  }
+}
+
+const start = new Vector(30, 50);
+const moveTo = new Vector(5, 10);
+const finish = start.plus(moveTo.times(2));
+
+console.log(`Исходное расположение: ${start.x}:${start.y}`);
+console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
