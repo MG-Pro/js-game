63f3992ce2099e0d228189bbfd0dde73362bc3e6
MG <mg84@bk.ru>
2017-09-21 23:20:12 +0300

    class Actor in work

--- js-game-1.2.0/game.js
+++ js-game-1.2.0/game.js
@@ -27,11 +27,67 @@ class Vector {
   }
 }
 
-const start = new Vector(30, 50);
-const moveTo = new Vector(5, 10);
-const finish = start.plus(moveTo.times(2));
-let a = {};
-start.plus(a);
+class Actor {
+  constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
+    if(!(pos instanceof Vector && size instanceof Vector && speed instanceof Vector)) {
+      throw new Error(`Аргумент не является прототипом Vector`);
+    }
+    this.pos = pos;
+    this.size = size;
+    this.speed = speed;
+    Object.defineProperty(this, 'left', {
+      get: () => {
+        return this.pos.x;
+      }
+    });
+    Object.defineProperty(this, 'top', {
+      get: () => {
+        return this.pos.y;
+      }
+    });
+    Object.defineProperty(this, 'right', {
+      get: () => {
+        return this.pos.x + this.size.x;
+      }
+    });
+    Object.defineProperty(this, 'bottom', {
+      get: () => {
+        return this.pos.y + this.size.y;
+      }
+    });
+    Object.defineProperty(this, 'type', {
+      value: 'actor'
+      }
+    );
+  }
+  act() {
+    return true;
+  }
 
-console.log(`Исходное расположение: ${start.x}:${start.y}`);
-console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
+  isIntersect(actor) {
+    if(!(actor instanceof Actor)) {
+      throw new Error(`Аргумент не является прототипом Actor`);
+    }
+    if(this.left === actor.left && this.top === actor.top && this.right === actor.right && this.bottom === actor.bottom) {
+      return false;
+    }
+
+    if(this.left > actor.right)
+      return true;
+    else if(this.top < actor.bottom)
+      return true;
+    else if(this.right < actor.left)
+      return true;
+    else if(this.bottom < actor.top)
+      return true;
+    else
+      return false;
+
+  }
+}
+let position = new Vector(10,10);
+let size = new Vector(1,1);
+const player = new Actor(new Vector(0, 0));
+const coin = new Actor(new Vector(100, 100));
+const notIntersected = player.isIntersect(coin);
+
